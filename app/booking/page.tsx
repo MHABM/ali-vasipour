1238d4b152b997a8dc03f8f29f8d1d5a0aac3f39
MHABM <mhabm2th@gmail.com>
2024-04-20 22:01:11 +0330

    finish booking

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -10,7 +10,7 @@ export default function Page({searchParams}:{searchParams:{day:string,hour:strin
     const day = persianDate(searchParams.day)
     const hour = searchParams.hour
     const {register,handleSubmit,reset,formState:{errors}}=useForm()
-    const [stepTwo, setStepTwo] = useState(false)
+    const [stepTwo, setStepTwo] = useState(true)
     const [code, setCode] = useState('')
     const [message,setMessage] = useState('')
     const [firstName,setFirstName] = useState('')
@@ -91,7 +91,8 @@ export default function Page({searchParams}:{searchParams:{day:string,hour:strin
                             <button className="text-white bg-lime-500 w-full rounded-md h-12 mt-5 mb-5 text-lg">تایید</button>
                         </form>
                         <div className='mt-14 flex flex-col justify-center items-center gap-4 font-bold text-gray-700 text-center'>
-                        {message && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 text-lime-500"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" /></svg>}
+                        {message && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#66c61c" className="w-20 h-20"><path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" /></svg>
+}
                         {message}
                         </div>
                     </div>
@@ -109,7 +110,7 @@ export default function Page({searchParams}:{searchParams:{day:string,hour:strin
                     </div>
                     <div className="flex flex-col">
                         <label className="mb-2 font-bold text-gray-700">شماره موبایل</label>
-                        <input {...register('phone',{pattern:{value: /^[\d٠١٢٣٤٥٦٧٨٩]{10,11}$/ }})} required inputMode='numeric' className=" border-gray-400 text-gray-900 rounded-md focus:border-lime-500 p-2.5 h-12 bg-transparent"></input>
+                        <input {...register('phone',{pattern:{value: /^[\d٠١٢٣٤٥٦٧٨٩]{10,11}$/ , message:''}})} required inputMode='numeric' className=" border-gray-400 text-gray-900 rounded-md focus:border-lime-500 p-2.5 h-12 bg-transparent"></input>
                         {(isError || errors.phone) && (<p className='text-sm mt-1 text-red-600'>شماره موبایل نادرست است.</p>)}
                     </div>
                     <button disabled={isSubmitting} className="text-white bg-lime-500 w-full rounded-md h-12 mt-10 mb-5 text-lg">
